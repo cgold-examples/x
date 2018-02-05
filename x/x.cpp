d592779a2803a43b0b2406aeca9c1b9ea114de52
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-02-05 10:43:07 +0300

    CMake code

---
+++ x/x.cpp
@@ -0,0 +1,11 @@
+#include <x/x.hpp>
+
+#include <iostream> // std::cout
+
+namespace x {
+
+void x::Run() {
+  std::cout << "x say: nice" << std::endl;
+}
+
+} // namespace x
---
+++ x/x.hpp
@@ -0,0 +1,13 @@
+#ifndef X_X_HPP_
+#define X_X_HPP_
+
+namespace x {
+
+class x {
+ public:
+  void Run();
+};
+
+} // namespace x
+
+#endif // X_X_HPP_
